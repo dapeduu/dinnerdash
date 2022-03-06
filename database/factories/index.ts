4cb053551d828034b2f260970857f1ec8688c244
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-03-06 17:15:01 -0300

    ✨ creates meal category

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,11 +1,18 @@
 // import Factory from '@ioc:Adonis/Lucid/Factory'
 import User from 'App/Models/User'
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import MealCategory from 'App/Models/MealCategory'
 
 export const UserFactory = Factory.define(User, ({ faker }) => {
   return {
     name: faker.name.findName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
+  }
+}).build()
+
+export const MealCategoryFactory = Factory.define(MealCategory, ({ faker }) => {
+  return {
+    name: faker.random.word(),
   }
 }).build()
